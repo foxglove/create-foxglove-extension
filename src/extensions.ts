8133ef887228265539e8aa4473721b887a41668b
John Hurliman <jhurliman@jhurliman.org>
2021-06-21 10:40:53 -0700

    `fox publish` command
    This command prints a JSON array entry for an extension given the URL to the released .foxe file. This JSON can be copy pasted directly into extensions.json in the studio-extension-marketplace repository.

---
+++ src/extensions.ts
@@ -0,0 +1,67 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+type ExtensionPackageJson = { name: string; version: string; publisher?: string };
+
+/**
+ * Returns a unique identifier for an extension based on the publisher and package name. The
+ * publisher can either be explicitly specified with a "publisher" field or extracted from the
+ * "name" field if it contains a namespace such as "@foxglove".
+ *
+ * This method will throw if any required fields are missing or invalid.
+ * @param pkgJson Parsed package.json file
+ * @returns An identifier string such as "foxglove.studio-extension-turtlesim"
+ */
+export function getPackageId(pkgJson: ExtensionPackageJson): string {
+  if (pkgJson == undefined) {
+    throw new Error(`Missing package.json`);
+  }
+
+  if (typeof pkgJson.name !== "string") {
+    throw new Error(`package.json is missing required "name" field`);
+  }
+  if (typeof pkgJson.version !== "string") {
+    throw new Error(`package.json is missing required "version" field`);
+  }
+
+  const pkgName = parsePackageName(pkgJson.name);
+  let publisher = pkgJson.publisher ?? pkgName.namespace;
+  if (publisher == undefined) {
+    throw new Error(`package.json is missing required "publisher" field`);
+  }
+
+  publisher = publisher.toLowerCase().replace(/\W+/g, "");
+  if (publisher.length === 0) {
+    throw new Error(`package.json contains an invalid "publisher" field`);
+  }
+
+  return `${publisher}.${pkgName.name}`;
+}
+
+/**
+ * Get the directory name to use for an installed extension
+ * @param pkgJson Parsed package.json file
+ * @returns A directory name such as "foxglove.studio-extension-turtlesim-1.0.0"
+ */
+export function getPackageDirname(pkgJson: ExtensionPackageJson): string {
+  const pkgId = getPackageId(pkgJson);
+  const dir = `${pkgId}-${pkgJson.version}`;
+  if (dir.length >= 255) {
+    throw new Error(`package.json publisher.name-version is too long`);
+  }
+  return dir;
+}
+
+/**
+ * Separate a package.json "name" field into separate namespace (i.e. @foxglove) and name fields
+ * @param name The "name" field from a package.json file
+ * @returns An object containing the unprefixed name and the namespace, if present
+ */
+export function parsePackageName(name: string): { namespace?: string; name: string } {
+  const res = /^@([^/]+)\/(.+)/.exec(name);
+  if (res == undefined) {
+    return { name };
+  }
+  return { namespace: res[1], name: res[2] as string };
+}
--- src/package.ts
+++ src/package.ts
@@ -1,13 +1,16 @@
 import { spawn } from "child_process";
+import { createHash } from "crypto";
 import { createReadStream, createWriteStream } from "fs";
 import { mkdir, readFile, readdir, stat } from "fs/promises";
 import JSZip from "jszip";
 import ncp from "ncp";
+import fetch from "node-fetch";
 import { homedir } from "os";
 import { join, normalize, relative, sep } from "path";
 import rimraf from "rimraf";
 import { promisify } from "util";
 
+import { getPackageDirname, getPackageId, parsePackageName } from "./extensions";
 import { info } from "./log";
 
 const cpR = promisify(ncp);
@@ -17,9 +20,15 @@ const cpR = promisify(ncp);
 const MOD_DATE = new Date("2021-02-03");
 
 export interface PackageManifest {
+  id: string;
   name: string;
+  displayName: string;
+  description: string;
   publisher?: string;
   namespaceOrPublisher: string;
+  homepage?: string;
+  license?: string;
+  keywords?: string[];
   version: string;
   main: string;
   files?: string[];
@@ -35,6 +44,14 @@ export interface PackageOptions {
 
 export interface InstallOptions {
   readonly cwd?: string;
+}
+
+export interface PublishOptions {
+  foxe?: string;
+  cwd?: string;
+  version?: string;
+  readme?: string;
+  changelog?: string;
 }
 
 enum FileType {
@@ -71,6 +88,72 @@ export async function installCommand(options: InstallOptions = {}): Promise<void
   await install(files, extensionPath, pkg);
 }
 
+export async function publishCommand(options: PublishOptions): Promise<void> {
+  const foxeUrl = options.foxe;
+  if (foxeUrl == undefined) {
+    throw new Error(`--foxe <foxe> Published .foxe file URL is required`);
+  }
+
+  // Open the package.json file
+  const extensionPath = options.cwd ?? process.cwd();
+  const pkgPath = join(extensionPath, "package.json");
+  const pkg = await readManifest(extensionPath);
+
+  const publisher = pkg.namespaceOrPublisher;
+  if (publisher == undefined || publisher.length === 0 || publisher === "unknown") {
+    throw new Error(`Invalid publisher "${publisher}" in ${pkgPath}`);
+  }
+
+  const homepage = pkg.homepage;
+  if (homepage == undefined || homepage.length === 0) {
+    throw new Error(`Missing required field "homepage" in ${pkgPath}`);
+  }
+  const license = pkg.license;
+  if (license == undefined || license.length === 0) {
+    throw new Error(`Missing required field "license" in ${pkgPath}`);
+  }
+  const version = options.version ?? pkg.version;
+  if (version == undefined || version.length === 0) {
+    throw new Error(`Missing required field "version" in ${pkgPath}`);
+  }
+  if (version === "0.0.0") {
+    throw new Error(`Invalid version "${version}" in ${pkgPath}`);
+  }
+  const keywords = JSON.stringify(pkg.keywords ?? []);
+  const readme = options.readme ?? (await githubRawFile(homepage, "README.md"));
+  if (readme == undefined || readme.length === 0) {
+    throw new Error(`Could not infer README.md URL. Use --readme <url>`);
+  }
+  const changelog = options.changelog ?? (await githubRawFile(homepage, "CHANGELOG.md"));
+  if (changelog == undefined || changelog.length === 0) {
+    throw new Error(`Could not infer CHANGELOG.md URL. Use --changelog <url>`);
+  }
+
+  // Fetch the .foxe file and compute the SHA256 hash
+  const res = await fetch(foxeUrl);
+  const foxeData = await res.arrayBuffer();
+  const hash = createHash("sha256");
+  const sha256sum = hash.update(new Uint8Array(foxeData)).digest("hex");
+
+  // Print the extension.json entry
+  info(`
+  {
+    "id": "${pkg.id}",
+    "name": "${pkg.displayName ?? pkg.name}",
+    "description": "${pkg.description}",
+    "publisher": "${pkg.namespaceOrPublisher}",
+    "homepage": "${homepage}",
+    "readme": "https://raw.githubusercontent.com/foxglove/studio-extension-turtlesim/main/README.md",
+    "changelog": "https://raw.githubusercontent.com/foxglove/studio-extension-turtlesim/main/CHANGELOG.md",
+    "license": "${license}",
+    "version": "${version}",
+    "sha256sum": "${sha256sum}",
+    "foxe": "${foxeUrl}",
+    "keywords": ${keywords}
+  }
+`);
+}
+
 async function readManifest(extensionPath: string): Promise<PackageManifest> {
   const pkgPath = join(extensionPath, "package.json");
   let pkg: unknown;
@@ -99,6 +182,7 @@ async function readManifest(extensionPath: string): Promise<PackageManifest> {
     throw new Error(`Unknown publisher, add a "publisher" field to package.json`);
   }
   manifest.namespaceOrPublisher = publisher;
+  manifest.id = getPackageId(manifest);
 
   return manifest;
 }
@@ -252,21 +336,29 @@ function addFileToZip(zip: JSZip, baseDir: string, filename: string) {
   zip.file<"stream">(filename, createReadStream(fullPath), { createFolders: true, date: MOD_DATE });
 }
 
-function getPackageDirname(pkg: PackageManifest): string {
-  const pkgName = parsePackageName(pkg.name);
-  return `${pkg.namespaceOrPublisher}.${pkgName.name}-${pkg.version}`;
-}
-
-function parsePackageName(name: string): { namespace?: string; name: string } {
-  const res = /^@([^/]+)\/(.+)/.exec(name);
-  if (res == undefined) {
-    return { name };
-  }
-  return { namespace: res[1], name: res[2] as string };
-}
-
 function inDirectory(directory: string, pathname: string): boolean {
   const relPath = relative(directory, pathname);
   const parts = relPath.split(sep);
   return parts[0] !== "..";
+}
+
+async function githubRawFile(homepage: string, filename: string): Promise<string | undefined> {
+  const match = /^https:\/\/github\.com\/([^/]+)\/([^/?]+)$/.exec(homepage);
+  if (match == undefined) {
+    return undefined;
+  }
+
+  const [_, org, project] = match;
+  if (org == undefined || project == undefined) {
+    return undefined;
+  }
+
+  const url = `https://raw.githubusercontent.com/${org}/${project}/main/${filename}`;
+  try {
+    const res = await fetch(url);
+    const content = await res.text();
+    return content.length > 0 ? url : undefined;
+  } catch {
+    return undefined;
+  }
 }
