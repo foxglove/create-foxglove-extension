806114ae4ad36b9c77bf5b615bc6404399687439
Adrian Macneil <adrian@foxglove.dev>
2022-07-05 17:13:17 -0700

    Rename package to create-foxglove-extension (#59)

---
+++ src/bin/create-foxglove-extension.ts
@@ -0,0 +1,14 @@
+import { program } from "commander";
+
+import { createCommand } from "..";
+
+program
+  .description("Creates a new Foxglove Studio extension")
+  .showHelpAfterError()
+  .argument("<name>", "Name for the new extension")
+  .allowExcessArguments(false)
+  .action(async (name: string) => {
+    return await createCommand({ name });
+  });
+
+program.parse(process.argv);
