0f44b50f5a08b477d2690d04f848799ae4ce4c77
Adrian Macneil <adrian@foxglove.dev>
2022-07-05 21:54:22 -0700

    Add missing shebang to bin scripts (#61)

--- src/bin/foxglove-extension.ts
+++ src/bin/foxglove-extension.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { program, Option } from "commander";
 
 import { buildCommand, installCommand, packageCommand, publishCommand, PublishOptions } from "..";
@@ -7,48 +8,46 @@ function main(task: Promise<void>): void {
   task.catch(fatal);
 }
 
-module.exports = function (argv: string[]): void {
-  program.usage("<command> [options]");
+program.usage("<command> [options]");
 
-  program
-    .command("build")
-    .description("Build an extension, preparing it for packaging or installation")
-    .addOption(new Option("--mode [mode]", "Build mode").choices(["development", "production"]))
-    .option("--cwd [cwd]", "Directory to run the build command in")
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    .action(({ mode, cwd }) => main(buildCommand({ mode, cwd })));
+program
+  .command("build")
+  .description("Build an extension, preparing it for packaging or installation")
+  .addOption(new Option("--mode [mode]", "Build mode").choices(["development", "production"]))
+  .option("--cwd [cwd]", "Directory to run the build command in")
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  .action(({ mode, cwd }) => main(buildCommand({ mode, cwd })));
 
-  program
-    .command("package")
-    .description("Packages an extension")
-    .option("-o, --out [path]", "Output .foxe extension file to [path] location")
-    .option("--cwd [cwd]", "Directory to run the package command in")
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    .action(({ out, cwd }) => main(packageCommand({ packagePath: out, cwd })));
+program
+  .command("package")
+  .description("Packages an extension")
+  .option("-o, --out [path]", "Output .foxe extension file to [path] location")
+  .option("--cwd [cwd]", "Directory to run the package command in")
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  .action(({ out, cwd }) => main(packageCommand({ packagePath: out, cwd })));
 
-  program
-    .command("install")
-    .description("Locally installs an extension")
-    .option("--cwd [cwd]", "Directory to run the install command in")
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    .action(({ cwd }) => main(installCommand({ cwd })));
+program
+  .command("install")
+  .description("Locally installs an extension")
+  .option("--cwd [cwd]", "Directory to run the install command in")
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  .action(({ cwd }) => main(installCommand({ cwd })));
 
-  program
-    .command("publish")
-    .description(
-      "Create an extensions.json entry for a released extension. This can be added to the https://github.com/foxglove/studio-extension-marketplace repository",
-    )
-    .option("--foxe <foxe>", "URL of the published .foxe file")
-    .option("--cwd [cwd]", "Directory containing the extension package.json file")
-    .option("--version [version]", "Version of the published .foxe file")
-    .option("--readme [readme]", "URL of the extension README.md file")
-    .option("--changelog [changelog]", "URL of the extension CHANGELOG.md file")
-    .action((options: PublishOptions) => main(publishCommand(options)));
+program
+  .command("publish")
+  .description(
+    "Create an extensions.json entry for a released extension. This can be added to the https://github.com/foxglove/studio-extension-marketplace repository",
+  )
+  .option("--foxe <foxe>", "URL of the published .foxe file")
+  .option("--cwd [cwd]", "Directory containing the extension package.json file")
+  .option("--version [version]", "Version of the published .foxe file")
+  .option("--readme [readme]", "URL of the extension README.md file")
+  .option("--changelog [changelog]", "URL of the extension CHANGELOG.md file")
+  .action((options: PublishOptions) => main(publishCommand(options)));
 
-  program.on("command:*", ([_cmd]: string) => {
-    program.outputHelp({ error: true });
-    process.exit(1);
-  });
+program.on("command:*", ([_cmd]: string) => {
+  program.outputHelp({ error: true });
+  process.exit(1);
+});
 
-  program.parse(argv);
-};
+program.parse(process.argv);
