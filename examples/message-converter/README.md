e1f7be4de4eb195aece717d5d11775d2dbacc970
Roman Shtylman <roman@foxglove.dev>
2022-11-29 09:42:11 -0800

    Add message converter example (#87)
    Fixes: #85

---
+++ examples/message-converter/README.md
@@ -0,0 +1,6 @@
+# Message Converter Example
+
+Example [Message Converter extension](https://foxglove.dev/docs/studio/extensions/getting-started#message-converters)
+
+This example shows how to convert a custom message from your own framework into a message supported
+and visualized by Studio.
---
+++ examples/message-converter/src/index.ts
@@ -0,0 +1,23 @@
+import { ExtensionContext } from "@foxglove/studio";
+import { LocationFix } from "@foxglove/schemas";
+
+type MyGps = {
+  lat: number;
+  lon: number;
+};
+
+// activate is the entry point for our entire extension.
+export function activate(extensionContext: ExtensionContext): void {
+  // Register a message converter from our custom Gps message type to `foxglove.LocationFix`
+  // This will enable Foxglove Studio to visualize our custom Gps message in the Map panel.
+  extensionContext.registerMessageConverter({
+    fromSchemaName: "My.Gps",
+    toSchemaName: "foxglove.LocationFix",
+    converter: (msg: MyGps): Partial<LocationFix> => {
+      return {
+        latitude: msg.lat,
+        longitude: msg.lon,
+      };
+    },
+  });
+}
