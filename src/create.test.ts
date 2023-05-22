6adde30cc9f327af4ecd257b2032efaf39ef54fb
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2023-05-22 14:18:33 +0700

    Don't generated unneeded .d.ts files in extension builds

--- src/create.test.ts
+++ src/create.test.ts
@@ -48,5 +48,11 @@ describe("createCommand", () => {
 
     // make sure the skeleton package is buildable and packagable
     await packageCommand({ cwd: destDir });
+
+    // make sure we don't generate unneeded .d.ts files
+    const builtContents = await readdir(path.join(destDir, "dist"), { withFileTypes: true });
+    const builtFiles = builtContents.filter((entry) => entry.isFile()).map((entry) => entry.name);
+    expect(builtFiles).not.toContain("ExamplePanel.d.ts");
+    expect(builtFiles).not.toContain("index.d.ts");
   });
 });
