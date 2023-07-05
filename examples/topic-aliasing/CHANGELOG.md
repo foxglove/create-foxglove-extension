8b310dcd120bdd84a9e1047e45cff55d2ac12022
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2023-07-05 06:58:38 -0600

    Add topic mapping example (#107)
    ### Public-Facing Changes
    Add topic mapping extension example.
    
    ### Description
    Add topic mapping extension example. This works with nuscenes data.

---
+++ examples/topic-aliasing/CHANGELOG.md
@@ -0,0 +1,5 @@
+# topic-aliasing-extension version history
+
+## 1.0.0
+
+- Initial version
---
+++ examples/topic-aliasing/src/index.ts
@@ -0,0 +1,23 @@
+import { ExtensionContext } from "@foxglove/studio";
+
+export function activate(extensionContext: ExtensionContext): void {
+  // Register a topic alias function that takes the current list of datasource topics and
+  // global variables and outputs a list of topic aliases.
+  extensionContext.registerTopicAliases(({ topics, globalVariables }) => {
+    // Output a list of aliased topics, in this case influenced by the current value of
+    // the global variable `device`.
+    const camera = globalVariables["camera"] ?? "FRONT";
+    const bulkAliasedTopics = topics.map((topic) => {
+      return {
+        sourceTopicName: topic.name,
+        name: `/bulk_aliases${topic.name.toLowerCase()}`,
+      };
+    });
+    return [
+      { sourceTopicName: `/CAM_${camera}/image_rect_compressed`, name: `/selected_camera_image` },
+      { sourceTopicName: "/imu", name: "/aliased_imu" },
+      { sourceTopicName: "/odom", name: "/aliased_odom" },
+      ...bulkAliasedTopics,
+    ];
+  });
+}
